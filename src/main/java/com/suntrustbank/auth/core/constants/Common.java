6bd40acc99d70bc65c2ec1f73e6444277ffc29d8
Ifechigo <dada.ifeoluwani@yahoo.com>
2024-12-15 12:27:56 +0100

    removed unused constants

--- src/main/java/com/suntrustbank/auth/core/constants/Common.java
+++ src/main/java/com/suntrustbank/auth/core/constants/Common.java
@@ -15,15 +15,4 @@ public class Common {
     public static final String DEFAULT_OTP = "000111";
     public static final String PRODUCTION = "prod";
     public static final String STAGING = "staging";
-
-
-//    public static final String SENDER_EMAIL = "no-reply@getrova.com";
-//    public static final String VERIFY_OTP = "Verify OTP";
-//    public static final String WELCOME_TO_BAAS = "Welcome to SunTrust";
-//    public static final String RESEND_EMAIL = "rs";
-//    public static final int RESEND_EMAIL_REFERENCE_LENGTH = 3;
-//    public static final String EMAIL = "email";
-//    public static final String USER_ID = "userId";
-//    public static final String PIN = "pin";
-//    public static final String PHONE = "phone_";
 }
